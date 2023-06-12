e16814c0bcf20c28ddee52ca08fbe69dc3617c75
Nguyen Phong <105875447+Jen-ncode@users.noreply.github.com>
2023-06-12 21:25:15 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Cart from "./Pages/Cart";
+import Home from "./Pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+
+const App = () => {
+  return <Cart/>;
+};
+
+export default App;
